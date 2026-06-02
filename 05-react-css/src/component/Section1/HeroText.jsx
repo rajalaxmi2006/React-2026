1bce0556795384c340c5787214fe5fa7d498acff
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-02 13:21:06 +0530

    react-css

---
+++ 05-react-css/src/component/Section1/HeroText.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <div className="p-6">
+      <h3 className="mb-7 text-7xl font-bold">
+        Leadership <br />
+        <span className="text-blue-500">Excellence</span> <br />
+        Portal
+      </h3>
+      <p className="text-xl w-[90%] font-medium text-gray-600">
+       Our leadership team combines technical expertise, strategic vision, and operational excellence to deliver innovative digital solutions. Each member brings unique experience in technology, business growth, and project execution, helping organizations achieve measurable results in a rapidly evolving digital landscape.
+      </p>
+    </div>
+  );
+};
+
+export default HeroText;
---
+++ 05-react-css/src/data.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQHAP4gynqQkgQ/profile-displayphoto-scale_400_400/B56Z4vY8fuGoAg-/0/1778911531392?e=1781740800&v=beta&t=ZjeOnFiauDRgvbhL9bs4EUHgXlNtXQMhgzWmEGo3ITg",
+    intro: "",
+    color: "royalblue",
+    tag: "CTO",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQEYsIU0KphAyg/profile-displayphoto-scale_400_400/B56ZiDU8kkHkAg-/0/1754549964122?e=1781740800&v=beta&t=GcyxytiOjRVv07ymfeLrl1ZtZ5RY-9QNCeogeBin3ts",
+    color: "lightseagreen",
+    intro: "",
+    tag: "Chairman",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQFLFCYAuULh2Q/profile-displayphoto-crop_800_800/B56Z1DprUxK0AI-/0/1774956496214?e=1781740800&v=beta&t=4gKhUbNCt6qt7QCGuJLBfZ44j3wvR1BY92fddrXXv34",
+    color: "orange",
+    intro: "",
+    tag: "Placement Co-ordinator",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5635AQE3zOqRpvczGg/profile-framedphoto-shrink_800_800/B56ZqA30ZuG4Ak-/0/1763098707846?e=1780988400&v=beta&t=KhagQM5o_zo-d6drF9xavjYwchUupjU-VxNR9BXQoFs",
+    color: "purple",
+    intro: "",
+    tag: "Vice-President",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQG6o_Wcmf2ASg/profile-displayphoto-shrink_400_400/B56ZRWdpvQH0Ag-/0/1736617402895?e=1781740800&v=beta&t=fauvB7do9Y-zydqAe1D0Ycia92DRZBTwAuPUCt270To",
+    intro: "",
+    color: "black",
+    tag: "President",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQEYO0i5ILt--A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1701048744153?e=1781740800&v=beta&t=ho6q7529uCXQKfCK1vzx5kZ6RxUyrchEGJdXiPKFiaU",
+    intro: "",
+    color: "orange",
+    tag: "Academic Director",
+  },
+];
+
+export default users;
