f0c864f3f6b742e2312d08554eaf4dcedac124be
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-30 06:10:14 +0530

    notes app

---
+++ 08-react-notes/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <h1>
+                📝React Notes Keeper
+            </h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
---
+++ 08-react-notes/src/components/NoteForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const NoteForm = () => {
+
+    const [title,setTitle] = useState("");
+
+    const [desc,setDesc] = useState("");
+
+    function handelChange(e){
+        setTitle(e.target.value)
+        console.log(e.target.value);
+        
+    }
+
+  return (
+    <div>
+        <input type="text" placeholder='Enter Title' value={title} type="text" onChange={handelChange} />
+        <textarea placeholder='Enter Description' value={desc}></textarea>
+        <button>Add Notes</button>
+    </div>
+  )
+}
+
+export default NoteForm
