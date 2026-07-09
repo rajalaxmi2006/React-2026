fbbe8ecc65a7aded9dacd9ecb0adf2d7f8e4b4fa
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-07-09 12:29:45 +0530

    update

--- 09-react-effects/src/App.jsx
+++ 09-react-effects/src/App.jsx
@@ -1,16 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  
+  // const [count, setCount] = useState(0);
 
-  const [count,setCount]=useState(0);
-  console.log(count);
-  
+  const [name, setName] = useState("");
+
+  //  function handelClick(){
+  //   setCount(count+1);
+  //  }
+  //  console.log(count);
+  //  document.title= `react-effect ${count}`;
+
+  // useEffect(() => {
+
+  //   console.log("useEffect Activated!!");
+  //   document.title = `09-react-effects (${count}) `;
+
+  // }, [count]);
+
+  useEffect(() => {
+    document.title = name;
+  }, [name]);
+
+  const handelClick = () => {
+    alert(name);
+  };
+
   return (
     <div>
-      <h1>Side Effects in React</h1>
-    </div>
-  )
-}
+      {/* <h1>Side Effects in React</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button> */}
 
-export default App 
+      <input
+        type="text"
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default App;
