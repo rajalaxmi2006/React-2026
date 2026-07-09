d2fe582b04fc040a14771a42f27eaf1da741d4a6
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-07-09 11:07:03 +0530

    react-effects

---
+++ 09-react-effects/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+
+  console.log("Start");
+  document.title= "react-effects";
+  console.log("title");
+  
+  return (
+    <div>
+      <h1>Side Effects in React</h1>
+    </div>
+  )
+}
+
+export default App 
